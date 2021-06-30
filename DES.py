c1420f8d8e0c9c4e4a2b25baf65943f063584387
ckx-sec <56886249+ckx-sec@users.noreply.github.com>
2021-06-30 11:34:28 +0800

    Update DES.py

--- DES.py
+++ DES.py
@@ -1,49 +1,27 @@
 from DES_dic import *
+from Crypto.Util.number import *
 import re
-import base64
 
 
-def write_in_file(str_mess):
+def write_file(str_mess):
     try:
-        f = open('DES.txt', 'w', encoding='utf-8')
-        f.write(str_mess)
+        f = open('DES.txt', 'w')
+        f.write("{}".format(str_mess))
         f.close()
         print("文件输出成功！")
     except IOError:
         print('文件加解密出错！！！')
 
 
-def read_out_file():
+def read_file():
     try:
-        f = open('DES.txt', 'r', encoding='utf-8')
+        f = open('DES.txt', 'r')
         mess = f.read()
         f.close()
         print("文件读取成功！")
         return mess
     except IOError:
         print('文件加解密出错！！！')
-
-
-# 字符串转化为二进制
-def str2bin(message):
-    res = ""
-    for i in message:
-        tmp = bin(ord(i))[2:]
-        for j in range(0, 8-len(tmp)):
-            tmp = '0' + tmp  # 把输出的b给去掉
-        res += tmp
-    return res
-
-
-# 二进制转化为字符串
-def bin2str(bin_str):
-    res = ""
-    tmp = re.findall(r'.{8}', bin_str)
-    for i in tmp:
-        res += chr(int(i, 2))
-    return res
-    # print("未经过编码的加密结果:"+res)
-    # print("经过base64编码:"+str(base64.b64encode(res.encode('utf-8')),'utf-8'))
 
 
 # IP盒处理
@@ -156,7 +134,7 @@ def gen_key(key):
     return key_list
 
 
-def des_encrypt_one(bin_message, bin_key):  # 64位二进制加密的测试
+def encrypt_64bits(bin_message, bin_key):  # 64位二进制加密的测试
     #bin_message = deal_mess(str2bin(message))
     mes_ip_bin = ip_change(bin_message)
     #bin_key = input_key_judge(str2bin(key))
@@ -177,9 +155,8 @@ def des_encrypt_one(bin_message, bin_key):  # 64位二进制加密的测试
 # 64位二进制解密的测试,注意秘钥反过来了，不要写错了
 
 
-def des_decrypt_one(bin_mess, bin_key):
+def decrypt_64bits(bin_mess, bin_key):
     mes_ip_bin = ip_change(bin_mess)
-    #bin_key = input_key_judge(str2bin(key))
     key_lst = gen_key(bin_key)
     lst = range(1, 16)
     cipher_left = mes_ip_bin[0:32]
@@ -192,56 +169,34 @@ def des_decrypt_one(bin_mess, bin_key):
     fin_right = cipher_right
     fin_output = fin_left + fin_right
     bin_plain = ip_re_change(fin_output)
-    res = bin2str(bin_plain)
+    res = str(long_to_bytes(int(bin_plain, 2)), encoding="utf-8")
     return res
 
 
-# 简单判断以及处理信息分组
-def deal_mess(bin_mess):
-    """  
-    :param bin_mess: 二进制的信息流
-    :return: 补充的64位信息流
-    """
-    ans = len(bin_mess)
-    if ans % 64 != 0:
-        for i in range(64 - (ans % 64)):  # 不够64位补充0
-            bin_mess += '0'
-    return bin_mess
-
-
-# 查看秘钥是否为64位
-def input_key_judge(bin_key):
-    """
-    全部秘钥以补0的方式实现长度不满足64位的
-    :param bin_key: 
-    """
-    ans = len(bin_key)
-    if len(bin_key) < 64:
-        if ans % 64 != 0:
-            for i in range(64 - (ans % 64)):  # 不够64位补充0
-                bin_key += '0'
-    # else:
-    #     bin_key = bin_key[0:64]    #秘钥超过64位的情况默认就是应该跟密文一样长 直接将密钥变为跟明文一样的长度，虽然安全性会有所下降
-    return bin_key
-
-
-def all_message_encrypt(message, key):
-    bin_mess = deal_mess(str2bin(message))
+def des_encrypt(message, key):
+    message = bytes(message, encoding="utf8")
+    message = bytes_to_long(message)
+    bin_message = "{0:064b}".format(message)
+    key = bytes(key, encoding="utf8")
+    key = bytes_to_long(key)
+    bin_key = "{0:064b}".format(key)
+    tmp = re.findall(r'.{64}', bin_message)
     res = ""
-    bin_key = input_key_judge(str2bin(key))
-    tmp = re.findall(r'.{64}', bin_mess)
     for i in tmp:
-        res += des_encrypt_one(i, bin_key)
+        res += encrypt_64bits(i, bin_key)
     return res
 
 
-def all_message_decrypt(message, key):
-    bin_mess = deal_mess(str2bin(message))
+def des_decrypt(message, key):
+    bin_message = "{0:064b}".format(int(message))
+    key = bytes(key, encoding="utf8")
+    key = bytes_to_long(key)
+    bin_key = "{0:064b}".format(key)
+
     res = ""
-    bin_key = input_key_judge(str2bin(key))
-    tmp = re.findall(r'.{64}', bin_mess)
+    tmp = re.findall(r'.{64}', bin_message)
     for i in tmp:
-        res += des_decrypt_one(i, bin_key)
+        res += decrypt_64bits(i, bin_key)
     return res
 
 
@@ -254,20 +209,19 @@ def get_mode():
         message = input().replace(' ', '')
         print("请输入你的秘钥：")
         key = input().replace(' ', '')
-        s = all_message_encrypt(message, key)
-        out_mess = bin2str(s)
-        print("加密过后的内容:" + out_mess)
-        write_in_file(out_mess)
-        # print(type(out_mess))
-        # base_out_mess = base64.b64encode(out_mess.encode('utf-8'))
-        # print("Base64编码过后:"+ base_out_mess.decode())
+        s = des_encrypt(message, key)
+        out_mess = int(s, 2)
+        print("密文长整型:")
+        print(out_mess)
+        write_file(out_mess)
+
     elif mode == '2':
         # print("请输入信息输入字符串不能为空：")
         # message = input().replace(' ', '')
         print("请输入你的秘钥：")
         key = input().replace(' ', '')
-        message = read_out_file()
-        s = all_message_decrypt(message, key)
+        message = read_file()
+        s = des_decrypt(message, key)
         #out_mess = bin2str(s)
         print("解密后的信息：" + s)
     else:
